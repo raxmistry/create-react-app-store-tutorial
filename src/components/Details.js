a2cc390505e4102ec387ef4fbd895ad10d75b1a4
Rakesh Mistry <rakesh@qualica.com>
2019-02-27 22:02:21 +0200

    Basic components created

---
+++ src/components/Details.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Details extends React.Component {
+	render() {
+		return (
+			<div>
+			<h3> hello from details </h3>
+			</div>
+		);
+	}
+}

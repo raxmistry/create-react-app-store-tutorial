eb1b381eb795fa0a0178521a22e9cccca4dfdfe6
Rakesh Mistry <rakesh@qualica.com>
2019-03-13 21:49:39 +0200

    Products added at toplevel.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,7 +7,7 @@ import {ButtonContainer} from './Button';
 export default class NavBar extends Component {
     render() {
         return (
-            <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+            <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
                 <Link to='/'>
                     <img src={logo} alt="store" className="navbar-brand" />
                 </Link>
@@ -26,10 +26,20 @@ export default class NavBar extends Component {
                         My Cart
                     </ButtonContainer>
                 </Link>
-            </nav>
+            </NavWrapper>
         );
 
     }
 }
+
+
+const NavWrapper =  styled.nav`
+  background: var(--mainBlue);
+  .nav-link {
+    color: var(--mainWhite)!important;
+    font-size:1.3rem;
+    text-transform: capitalize;
+  }
+`
 
 

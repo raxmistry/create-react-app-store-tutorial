adb4ab1347d08f0f32bcf1c365dbba70030cb877
Rakesh Mistry <rakesh@qualica.com>
2019-04-17 21:21:10 +0200

    Added detail page with info and non working buttons

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import {ProductConsumer} from '../context';
+import PropTypes from 'prop-types';
 
 export default class Product extends React.Component {
     render() {
@@ -40,6 +41,16 @@ export default class Product extends React.Component {
             </ProductWrapper>
         );
     }
+}
+
+Product.propTypes = {
+  product:PropTypes.shape({
+      id:PropTypes.number,
+      img:PropTypes.string,
+      title:PropTypes.string,
+      price:PropTypes.number,
+      inCart:PropTypes.bool
+  }).isRequired
 }
 
 const ProductWrapper = styled.div`
